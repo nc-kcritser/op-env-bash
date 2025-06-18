94045cf77d0a9d69c13ef130ebc313f0bb610194
Ken Critser <34718929+dsskcritser@users.noreply.github.com>
2025-06-18 09:59:32 -0500

    Update basic-data-works.sh

--- basic-data-works.sh
+++ basic-data-works.sh
@@ -10,6 +10,26 @@ field() {
 	awk -F "${2:- }" "{ print \$${1:-1} }"
 }
 
+field() {
+    # Wrapper function for awk to get a field from a text, version 2 - augmented to remove white space
+    # Arg1: which field to get (default to 1)
+    # Arg2: what delimiter to use (default is whitespace)
+
+    local field_num="${1:-1}"
+    local delimiter="${2:- }" # If delimiter is a space, awk's default behavior handles multiple spaces.
+                               # If it's a specific character like '=', we need to trim explicitly.
+    awk -F "${delimiter}" -v field_num="${field_num}" '{
+        # Get the requested field
+        extracted_field = $field_num
+        # Remove leading whitespace from the extracted field
+        sub(/^[[:space:]]+/, "", extracted_field)
+        # Remove trailing whitespace from the extracted field
+        sub(/[[:space:]]+$/, "", extracted_field)
+        print extracted_field
+    }'
+}
+
+
 summarize() {
     local f=${1:-1}
     awk -F "${2:- }" "
